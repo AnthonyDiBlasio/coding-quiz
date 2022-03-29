5e9fe4de9f910d45f81e59f6cdb16d65510dc624
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-03-29 15:24:28 -0400

    adding html,css,adnd js files

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var startButton = document.querySelector(".start-button");
+var timerEl = document.querySelector("");
+var choiceEl = document.querySelector("");
+var answerEl = document.querySelector("");
+var solutionEl = document.querySelector("");
+var choice = ["hello", "goodbye","sdsds","sddadad"]
+
+function startQuiz(){
+
+}
